1ea0867789b1c97747bed2cd065e3c6ce7c12aa3
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-06 22:30:58 +0000

    added queries to sum time spent on assignments

---
+++ 1_queries/rollover_students.sql
@@ -0,0 +1,3 @@
+select students.name as student_name, students.start_date as student_start_date, cohorts.name as cohort_name, cohorts.start_date as cohort_start_date 
+FROM students LEFT JOIN cohorts ON cohorts.id = cohort_id 
+WHERE students.start_date <> cohorts.start_date; 
