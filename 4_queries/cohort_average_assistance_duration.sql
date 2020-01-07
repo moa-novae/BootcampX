ae00127a34ce780d4ed4cd9cbdfab506587ff731
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-07 19:44:48 +0000

    additional queries for assistance requests

---
+++ 4_queries/cohort_average_assistance_duration.sql
@@ -0,0 +1,8 @@
+SELECT avg(x.total_duration) as average_total_duration
+FROM (SELECT cohorts.name as cohort, sum(completed_at - started_at) as total_duration
+FROM students 
+JOIN cohorts ON students.cohort_id = cohorts.id 
+JOIN assistance_requests ON assistance_requests.student_id = students.id 
+GROUP BY cohorts.name
+ORDER BY total_duration
+)x;
