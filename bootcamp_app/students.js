43e23682b103a2ca349b5e817fb9e65cc63bb3ca
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-08 23:34:20 +0000

    queries are now parameterized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,17 @@ const pool = new Pool({
 });
 
 const myArgs = process.argv.slice(2);
+const value = [`%${myArgs[0]}%`, myArgs[1]];
 
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-where cohorts.name like '${myArgs[0]}%'
-LIMIT ${myArgs[1]};
-`)
+where cohorts.name like $1
+LIMIT $2;
+`;
+
+pool.query(queryString, value)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,18 @@ const pool = new Pool({
 });
 
 const myArgs = process.argv.slice(2);
-
-pool.query(`
+const value = [myArgs[0]];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers 
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id 
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name like '${myArgs[0] || 'JUL02'}'
+WHERE cohorts.name like $1
 ORDER BY teacher; 
-`)
+`;
+
+pool.query(queryString, value)
   .then(res => {
     res.rows.forEach(res => {
       console.log(`${res.cohort}: ${res.teacher}`);
